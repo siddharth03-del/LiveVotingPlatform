174a464996994f6fa4556fbf8b68225a5655e9da
siddharth03-del <siddharthsingh9361@gmail.com>
2025-03-08 19:11:31 +0530

    changed server configurations

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,21 @@
-from flask import Flask, request,jsonify
-from flask_socketio import SocketIO,send, emit
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import eventlet
+
 eventlet.monkey_patch()
 
 app = Flask(__name__)
 load_dotenv()
-app.config['SECKRET_KEY'] = 'Siddharthsingh'
+app.config['SECRET_KEY'] = 'Siddharthsingh'
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5173))
-    socketio.run(app, host='0.0.0.0', port=port)
+# Import after app initialization
 from Routes.api.api import api_route
 from Service.vote import CasteVoteService
-
 
 app.register_blueprint(api_route, url_prefix='/api')
 
@@ -38,17 +36,18 @@ def casteVoteHandler(json):
         emit('poll_update', response, broadcast=True, include_self=False)
     except Exception as e:
         print(e)
-        send({"message" : "There was an error casting vote"})
+        send({"message": "There was an error casting vote"})
 
 @socketio.on("remove_vote")
 def removeVoteHandler(json):
     from Controllers.vote import RemoveVote
     try:
-        print(json)
         response = RemoveVote(json)
-        print(response)
         emit('poll_update', response, broadcast=True, include_self=False)
     except Exception as e:
-        print("Error in handler of remove vote" ,e)
-        send({"message" : "There was an error removing vote"})
+        print("Error in handler of remove vote", e)
+        send({"message": "There was an error removing vote"})
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
