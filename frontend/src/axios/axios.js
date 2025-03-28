911a7a2edc3b6e52d115701f94476a7496b40ba6
siddharth03-del <siddharthsingh9361@gmail.com>
2025-03-28 16:20:40 +0530

    deployed new server

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const renderUrl = "https://livevotingplatform.onrender.com/api";
+export const renderUrl = "https://livevotingplatform-36v2.onrender.com/api";
 export const localUrl =  'http://192.168.100.111:5000/api';
 const axios_instance = axios.create({
     baseURL : renderUrl,
